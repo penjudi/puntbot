732f13b7716179c69c7cd816878084d9ccaccc54
penjudi <150814155+penjudi@users.noreply.github.com>
2023-11-15 19:17:43 +1000

    Create website_change_detection.py

---
+++ src/tests/website_change_detection.py
@@ -0,0 +1,8 @@
+# tests/test_mybot.py
+import unittest
+from mybot import MyBot
+
+class TestMyBot(unittest.TestCase):
+    def test_something(self):
+        # Write your test cases here
+        pass
